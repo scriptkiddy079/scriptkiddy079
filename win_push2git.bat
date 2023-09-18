0b8ac6e9954424f423adebd3429a668c10054cd5
scriptkiddy079 <prabhatkumarmishra.uoe@gmail.com>
2023-09-18 08:33:58 +0530

    Commit

---
+++ win_push2git.bat
@@ -0,0 +1,9 @@
+D:
+cd D:\At Github\scriptkiddy079
+del .git
+git lfs install 
+git init
+git add .
+git remote add origin https://github.com/scriptkiddy079/scriptkiddy079.git
+git commit -m "Commit"
+git push -f origin master
